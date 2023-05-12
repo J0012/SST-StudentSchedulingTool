1e8b827555435cc4e41c90f4c46f694aa2dedc33
mykahmarks <78816351+mykahmarks@users.noreply.github.com>
2023-05-12 10:03:22 -0700

    This code should match the functioning zip file and run

--- app/src/main/java/com/example/sstdatabase/databaseFunctionality/DataAPI.java
+++
@@ -1,7 +0,0 @@
-package com.example.sstdatabase.databaseFunctionality;
-
-import java.util.ArrayList;
-
-public class DataAPI {
-    ArrayList<DataAPIItem> dataAPIItem = new ArrayList<DataAPIItem>();
-}
